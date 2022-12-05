01730e9b34ca0a9b1a7488ba640585978b2eaff3
Aaron <aarpar204@gmail.com>
2022-12-05 13:45:50 -0600

    Add many more notes which are pushed into an array.

--- js/main.js
+++ js/main.js
@@ -1,70 +1,42 @@
 document.querySelector('#compose').addEventListener('click', compose)
 document.querySelector('#eights').addEventListener('click', eights)
 
-function compose () {
-	// Take in the notes
-	let allNotes = []
-	const fNote = document.querySelector('#firstNote').value
-	const secNote = document.querySelector('#secondNote').value
-	const thirdNote = document.querySelector('#thirdNote').value
-	const frthNote = document.querySelector('#fourthNote').value
-	
-	allNotes[0] = fNote
-	allNotes[1] = secNote
-	allNotes[2] = thirdNote
-	allNotes[3] = frthNote
-	allNotes.toString()
-	
-	let noteRegex = /^[A-G][-#]\d/i
+let allNotes = []
 
-	if (noteRegex.test(allNotes) == false) { 
-		alert('Your notes must be formatted with the note and octave like this: C-4 or C#4')
-	} else {
+function compose() {
+	// Take in the notes and store them in an array
+	let theNotes = document.querySelector('#theNotes').value
+	theNotes.toString()
+	allNotes = theNotes.split(',')
+	allNotes.push('...')
+	console.log(allNotes)
 	
-	//Composition happens here:
-
-	function compositionTime (numLines) {
-		allNotes[4] = '...'
-		allNotes[5] = '...'
-		allNotes[6] = '...'
-		allNotes[7] = '...'
+	//Compose
+	function compRand(numLines) {
 		document.querySelector('#displayNotes').innerText = ` `
 		document.querySelector('#displayNotes').innerText = `ModPlug Tracker  XM\n`
-    		for(let i = 1; i <= numLines; i++){
-        		document.querySelector('#displayNotes').innerText += `|${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n`
-    		}
+		for (i = 1; i <= numLines; i++) {
+				document.querySelector('#displayNotes').innerText += `|${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n`
+		}
 	}
-	
-	compositionTime (32) }
+	compRand(16)
 }
 
-function eights () {
-		// Take in the notes
-		let allNotes = []
-		const fNote = document.querySelector('#firstNote').value
-		const secNote = document.querySelector('#secondNote').value
-		const thirdNote = document.querySelector('#thirdNote').value
-		const frthNote = document.querySelector('#fourthNote').value
-		
-		allNotes[0] = fNote
-		allNotes[1] = secNote
-		allNotes[2] = thirdNote
-		allNotes[3] = frthNote
-		allNotes.toString()
-		
-		let noteRegex = /^[A-G][-#]\d/i
-
-	if (noteRegex.test(allNotes) == false) { 
-		alert('Your notes must be formatted with the note and octave like this: C-4 or C#4')
-	} else {
+function eights() {
+	// Take in the notes
+	let theNotes = document.querySelector('#theNotes').value
+	theNotes.toString()
+	allNotes = theNotes.split(',')
+	allNotes.push('...')
+	console.log(allNotes)
 	
-		function compEigths (numLines) {
-			document.querySelector('#displayNotes').innerText = ` `
-			document.querySelector('#displayNotes').innerText = `ModPlug Tracker  XM\n`
-				for(let i = 1; i <= numLines; i++){
-					document.querySelector('#displayNotes').innerText += `|${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n|...........\n`
-				}
+	//Compose
+	function compEigths(numLines) {
+		document.querySelector('#displayNotes').innerText = ` `
+		document.querySelector('#displayNotes').innerText = `ModPlug Tracker  XM\n`
+		for (i = 1; i <= numLines; i++) {
+				document.querySelector('#displayNotes').innerText += `|${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n|...........\n`
 		}
-	compEigths (16) 
 	}
+	compEigths(16)
 }
