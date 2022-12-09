7b8061df3343ee8c4ea266487c045477eccbbe28
Aaron <aarpar204@gmail.com>
2022-12-09 08:22:18 -0600

    ModPlug JS Composer v1.0

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # OpenMPT JS Tracker Music Maker
 
-This is a quick fun little Javascript app that can composes random music for OpenMPT Tracker. This project was created for the purpose of learning ES6. Simply enter some notes, select "Compose" or "Eights" for a random composition, scroll down, copy the resulting text starting from ModPlug Tracker XM, and paste it into a tracker channel in your OpenMPT project. (Note: currently works for .xm projects at this point).
-
-#Planned Features To Add (Depending on My Time as a Student)
-
-- drum slicing feature
-- rhythmic presets
-- use of more than one instrument in a channel
+This is a quick fun little Javascript app that can composes random music for OpenMPT Tracker. This project was created for the purpose of learning ES6. Simply enter some notes, select "Compose" or "Eights" for a random composition, scroll down, copy the resulting text starting from ModPlug Tracker XM, and paste it into a tracker channel in your OpenMPT project. (Note: currently works for .xm projects at this point). The rows do not apply with the EDM and Breakbeat presets. 
--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
 document.querySelector('#compose').addEventListener('click', compose)
-document.querySelector('#eights').addEventListener('click', eights)
 
 let allNotes = []
 const noteRegex = /^[A-G][-#]\d/i
+let numLines = 16
+let compOut = "rando"
 
-function compose() {
+function rando () {
 	// Take in the notes and store them in an array
 	let theNotes = document.querySelector('#theNotes').value
 	theNotes.toString()
@@ -30,10 +31,10 @@ function compose() {
 			document.querySelector('#displayNotes').innerText += `|${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n`
 		}
 	}
-	compRand(16)
+	compRand(numLines)
 }
 
-function eights() {
+function eigths() {
 	// Take in the notes and store them in an array
 	let theNotes = document.querySelector('#theNotes').value
 	theNotes.toString()
@@ -54,9 +55,64 @@ function eights() {
 	function compEigths(numLines) {
 		document.querySelector('#displayNotes').innerText = ` `
 		document.querySelector('#displayNotes').innerText = `ModPlug Tracker  XM\n`
-		for (i = 1; i <= numLines; i++) {
+		for (i = 1; i <= numLines - (numLines / 2); i++) {
 			document.querySelector('#displayNotes').innerText += `|${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n|...........\n`
 		}
 	}
-	compEigths(16)
+	compEigths (numLines)
+}
+
+function edm () {
+		// Take in the notes and store them in an array
+		let theNotes = document.querySelector('#theNotes').value
+		theNotes.toString()
+		allNotes = theNotes.split(',')
+		for (let i = 0; i < allNotes.length; i++) {
+			if (noteRegex.test(allNotes[i]) == false) {
+				document.querySelector('#displayNotes').innerText = 'Your notes must be formatted with the note and octave like this: C-4 or C#4. There must also be commas and no spaces after each note'
+				allNotes = theNotes.toString().split(',').filter(noteRegex.test)
+				return
+			} else {
+				document.querySelector('#displayNotes').innerText = ' '
+			}
+		}
+		console.log(allNotes)
+	
+		//Compose
+		function compEDM(numLines) {
+			document.querySelector('#displayNotes').innerText = ` `
+			document.querySelector('#displayNotes').innerText = `ModPlug Tracker  XM\n`
+			for (i = 1; i <= numLines; i++) {
+				document.querySelector('#displayNotes').innerText += `|${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n|...........\n|...........\n`
+			}
+		}
+		compEDM (numLines)
+			console.log('Rows do not matter here')
+}
+
+function breaks (numLines) {
+	// Declare break slice sample root notes and then screw and chop
+	allNotes = ['C-4','C#4','D-4','D#4']
+	document.querySelector('#displayNotes').innerText = ` `
+	document.querySelector('#displayNotes').innerText = `ModPlug Tracker  XM\n||${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n|...........\n|...........\n|...........\n||${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n|...........\n|...........\n|...........\n||${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n|...........\n|...........\n|...........\n||${allNotes[Math.floor(Math.random() * allNotes.length)]}01......\n|...........\n|...........\n|...........\n`
+}
+
+// Actual composition happens here
+function compose () {
+	compOut = document.querySelector('#presets');
+    preset = compOut.value;
+	console.log(preset)
+	if (preset === "rando") {
+		numLines = document.getElementById("rows").value;
+		rando ()
+	} else if (preset === "eigths") {
+		numLines = document.getElementById("rows").value;
+		eigths ()
+	} else if (preset === "edm") {
+		numLines = document.getElementById("rows").value;
+		edm ()
+	} else if (preset === "breaks") {
+		numLines = document.getElementById("rows").value;
+		breaks (numLines)
+	}
 }
